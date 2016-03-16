2fb2b1a98f94697da3550ed170f721129c4db132
Gregory Kanteres and Patrick Sullivan <patsul12@gmail.com>
2016-03-16 08:58:43 -0700

    Initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+  validates :username, presence: true
+  has_many :sentences
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
