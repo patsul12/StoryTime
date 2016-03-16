3e3a7e8a03b85977834d4b62bf3987b33bf9bb30
Gregory Kanteres and Patrick Sullivan <gregkanteres@gmail.com>
2016-03-16 14:13:30 -0700

    add test for deletion of sentences

--- spec/factories.rb
+++ spec/factories.rb
@@ -8,5 +8,17 @@ FactoryGirl.define do
   factory :story do
     title 'Test Title'
     user_id 1
+
+    factory :story_with_sentence do
+      after(:create) do |story|
+        create(:sentence, story: story)
+      end
+    end
+  end
+
+  factory :sentence do
+    user_id 1
+    content 'Test Sentence'
+    image_url Dir.new(Rails.root.to_s + "/app/assets/images").to_a.select{|f| f.downcase.match(/\.jpg|\.jpeg|\.png/) }.sample
   end
 end
--- spec/features/delete_sentence_spec.rb
+++ spec/features/delete_sentence_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 describe 'the delete a story process' do
   it 'deletes a story', js: true do
     user = FactoryGirl.create(:user)
-    story = FactoryGirl.create(:story)
+    story = FactoryGirl.create(:story_with_sentence)
     login_as(user, :scope => :user)
-    visit story_path(story)
+    visit story_sentence_path(story, story.sentences.first)
     page.evaluate_script('window.confirm = function() { return true; }')
-    click_link 'Delete Story'
-    expect(page).to have_content("Welcome")
+    click_link 'Delete Sentence'
+    expect(page).to have_content(story.title)
   end
 end
